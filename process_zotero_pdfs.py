39b603e69ca50b2ee416dd1f7f2c190e86318020
Pascal Calarco <pcalarco@uwindsor.ca>
2026-03-27 15:07:34 -0400

    Update process_zotero_pdfs.py
    Signed-off-by: Pascal Calarco <pcalarco@uwindsor.ca>

--- process_zotero_pdfs.py
+++ process_zotero_pdfs.py
@@ -8,9 +8,22 @@ After importing citations.ris into Zotero, you can select all of these citations
 
 These will be available in a new folder "Exported Items" in your original output folder.
 
-License: MIT License
-Copyright 2026 Pascal V. Calarco <pcalarco@uwindsor.ca>
-This script was developed with the help of Google Gemini Pro 3.1 and Microsoft Copilot
+Copyright (C) 2026 Pascal V. Calarco <pcalarco@uwindsor.ca>
+
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU Affero General Public License as
+ published by the Free Software Foundation, either version 3 of the
+ License. See https://www.gnu.org/licenses/agpl-3.0.en.html
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU Affero General Public License for more details.
+
+ You should have received a copy of the GNU Affero General Public License
+ along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+ This script was developed with the help of Google Gemini Pro 3.1 and Microsoft Copilot
 ----------------------------------------------------
 """
 
